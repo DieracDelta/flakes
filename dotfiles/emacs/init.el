437ed09c42f9cd3c2ebae7eeed390f7065c51057
Justin Restivo <justin.p.restivo@gmail.com>
2020-04-05 14:55:18 -0400

    added hydra keybinds

--- dotfiles/emacs/init.el
+++ dotfiles/emacs/init.el
@@ -1,20 +1,44 @@
-;(require 'evil-leader)
 (require 'hydra)
-;(defhydra hydra-zoom (global-map "<f2>")
-  ;"zoom"
-  ;("g" text-scale-increase "in")
-  ;("l" text-scale-decrease "out"))
-
 (require 'evil)
-;(global-evil-leader-mode)
-;(evil-leader/set-leader "<SPC>")
-;(evil-leader/set-key
-  ;"e"  'find-file
-  ;"b"  'switch-to-buffer
-  ;"k"  'kill-buffer
-  ;"bn" 'switch-to-next-buffer
-  ;)
 (evil-mode 1)
+;; must unbind normal minor mode keybind for space so can be used by hydra
+;; in doing so, unbindings insert mode, so must rebind
+(define-key evil-normal-state-map (kbd "SPC") 'hydra-space/body)
+(define-key evil-motion-state-map (kbd "SPC") 'self-insert-command)
+(define-key evil-insert-state-map (kbd "SPC") 'self-insert-command)
+;; have to turn this off
+(hydra-set-property 'hydra-space :verbosity 0)
+(hydra-set-property 'hydra-buffers :verbosity 0)
+(hydra-set-property 'hydra-windows :verbosity 0)
+;; spacebar bindings
+(defhydra hydra-space
+  (:hint nil)
+  ;("b" (if (minibufferp) ('self-insert-command) (hydra-buffers/body)) :exit t)
+  ("b" hydra-buffers/body :exit t)
+  ("w" hydra-windows/body :exit t)
+  ("<SPC>" execute-extended-command :exit t)
+  )
+
+;; buffer bindings
+(defhydra hydra-buffers
+  (:hint nil)
+  ("n" switch-to-next-buffer :exit t)
+  ("p" switch-to-prev-buffer :exit t)
+  ("d" kill-buffer :exit t)
+  )
+
+;; window bindings
+(defhydra hydra-windows
+  (:hint nil)
+  ("s" split-window-vertically :exit t)
+  ("v" split-window-horizontally :exit t)
+  ("d" delete-window :exit t)
+  ("l" evil-window-right :exit t)
+  ("h" evil-window-left :exit t)
+  ("k" evil-window-up :exit t)
+  ("j" evil-window-down :exit t)
+  )
+
 
 (require 'spacemacs-common)
 (deftheme spacemacs-dark "Spacemacs theme, the dark version")
@@ -24,3 +48,4 @@
 (menu-bar-mode -1)
 (toggle-scroll-bar -1)
 (tool-bar-mode -1)
+
