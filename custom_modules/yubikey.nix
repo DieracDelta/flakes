d572b35555e9c5789df7139cc8fe033981b96b47
Justin Restivo <justin@restivo.me>
2025-09-22 08:28:40 -0400

    feat: disable yubikey debug prints on sudo

--- custom_modules/yubikey.nix
+++ custom_modules/yubikey.nix
@@ -1,29 +1,39 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 let
   cfg = config.custom_modules.yubikey;
 in
 {
-  options.custom_modules.yubikey.enable =
-  lib.mkOption {
-      description = "Enable yubikey on the machine.";
-      type = lib.types.bool;
-      default = false;
+  options.custom_modules.yubikey.enable = lib.mkOption {
+    description = "Enable yubikey on the machine.";
+    type = lib.types.bool;
+    default = false;
   };
 
   config = lib.mkIf cfg.enable {
     security.pam.yubico = {
       enable = true;
-      debug = true;
+      debug = false;
       mode = "challenge-response";
     };
 
     # yubikey tools
     environment.systemPackages = with pkgs; [
-      gnupg pinentry
+      gnupg
+      pinentry
     ];
 
     # expose u2f
-    services.udev.packages = with pkgs; [ libu2f-host yubikey-personalization opensc pcsctools ];
+    services.udev.packages = with pkgs; [
+      libu2f-host
+      yubikey-personalization
+      opensc
+      pcsctools
+    ];
 
     # smartcard daemon
     services.pcscd.enable = true;
